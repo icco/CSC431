53ce91db81d1e89e7dc236f55f36cb6ed95065d8
Nat Welch <nat@natwelch.com>
2011-04-13 16:21:53 -0700

    Even cooler error messages.

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 # This is a test script for milestone 1.
-
+ERROR=""
 for file in $(ls tests/*.ev); do
-   java Evil $file
+   ERROR="$ERROR\n$( { java Evil $file; } 2>&1 )"
    if [ $? -eq 0 ]; then
-      echo "."
+      echo -n "."
    else
-      echo "F"
+      echo -n "F"
    fi
 done
+
+echo -e $ERROR
