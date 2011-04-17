0d7ec40b748e1d498b357a68ee358595135e9fab
Nat Welch <nat@natwelch.com>
2011-04-17 15:21:29 -0700

    Base idea for instructions.

--- src/Instruction.java
+++ src/Instruction.java
@@ -1,2 +1,1 @@
-public class Instruction {
-}
+public class Instruction {}
---
+++ src/MathInstruction.java
@@ -0,0 +1,10 @@
+/**
+ * The base for all of the Math type instructions. All of the subclasses of
+ * this class should be generated.
+ */
+public class MathInstruction extends Instruction {
+   public MathInstruction(String r1, String r2, String r3) {
+
+
+   }
+}
