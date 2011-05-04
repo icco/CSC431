f737559c976a75b76f6b8c55305550a98eafa053
Nat Welch <nat@natwelch.com>
2011-05-04 15:03:23 -0700

    Just moving the template code south.

--- generate_instructions.py
+++ generate_instructions.py
@@ -7,72 +7,6 @@
 
 from datetime import datetime, date, time
 
-iloc_txt = """import java.util.*;
-import java.lang.*;
-
-/**
- * Generated automatically by generate_instructions.py
- */
-public class %(classname)s extends Instruction {
-   public static Integer operandCount = %(count)d;
-   public %(classname)s() {
-      super();%(sparc)s
-   }
-
-   public String toString() {
-      return this.toILOC();
-   }
-
-   public String toSparc() {
-      return super.toString();
-   }
-
-   public String toILOC() {
-      String classPattern = new String("%(pattern)s");
-      String[] pattern = classPattern.split(" ");
-      String ret = "%(instr)s ";
-      int operandCount = this.getOperands().size();
-
-      if ((operandCount != 0) && (operandCount != pattern.length)) {
-         Evil.error(ret + ": Found " + operandCount + " operands, ILOC expecting " + pattern.length);
-      }
-
-      for (Operand r : this.getOperands()) {
-         if (!r.toString().equals(""))
-            ret = ret + r + ", ";
-      }
-
-      ret = ret.trim();
-      if (ret.lastIndexOf(",") == ret.length()-1)
-         ret = ret.substring(0, ret.length()-1);
-
-      for (int i = 0; i < this.getOperands().size(); i++) {
-         Operand o = this.getOperands().get(i);
-         String oper = "null";
-
-         if (o != null) { oper = o.getClass().getName(); }
-
-         if (!oper.equals(pattern[i])) {
-            Evil.error(ret + ": ILOC expecting " + classPattern + ". Found " + oper);
-         }
-      }
-
-      return ret;
-   }
-}
-"""
-
-sparc_txt = """import java.util.*;
-import java.lang.*;
-
-/**
- * Generated automatically by generate_instructions.py
- */
-public class %(classname)s extends SparcInstruction {
-
-}
-"""
-
 instructions = [
 
 # Arithmetic (MATHS)
@@ -365,6 +299,73 @@ instructions = [
       'sparc': [ ],
    },
 ]
+
+# Templates
+iloc_txt = """import java.util.*;
+import java.lang.*;
+
+/**
+ * Generated automatically by generate_instructions.py
+ */
+public class %(classname)s extends Instruction {
+   public static Integer operandCount = %(count)d;
+   public %(classname)s() {
+      super();%(sparc)s
+   }
+
+   public String toString() {
+      return this.toILOC();
+   }
+
+   public String toSparc() {
+      return super.toString();
+   }
+
+   public String toILOC() {
+      String classPattern = new String("%(pattern)s");
+      String[] pattern = classPattern.split(" ");
+      String ret = "%(instr)s ";
+      int operandCount = this.getOperands().size();
+
+      if ((operandCount != 0) && (operandCount != pattern.length)) {
+         Evil.error(ret + ": Found " + operandCount + " operands, ILOC expecting " + pattern.length);
+      }
+
+      for (Operand r : this.getOperands()) {
+         if (!r.toString().equals(""))
+            ret = ret + r + ", ";
+      }
+
+      ret = ret.trim();
+      if (ret.lastIndexOf(",") == ret.length()-1)
+         ret = ret.substring(0, ret.length()-1);
+
+      for (int i = 0; i < this.getOperands().size(); i++) {
+         Operand o = this.getOperands().get(i);
+         String oper = "null";
+
+         if (o != null) { oper = o.getClass().getName(); }
+
+         if (!oper.equals(pattern[i])) {
+            Evil.error(ret + ": ILOC expecting " + classPattern + ". Found " + oper);
+         }
+      }
+
+      return ret;
+   }
+}
+"""
+
+sparc_txt = """import java.util.*;
+import java.lang.*;
+
+/**
+ * Generated automatically by generate_instructions.py
+ */
+public class %(classname)s extends SparcInstruction {
+
+}
+"""
 
 # Now that the instructions are defined, apply them to the template.
 for instr in instructions:
