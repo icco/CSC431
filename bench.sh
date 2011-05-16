66185db8bff1ed63aef921e7d33bc91b70d45613
Nat Welch <nat@natwelch.com>
2011-05-16 14:27:43 -0700

    Trying to get bench.sh working.

--- bench.sh
+++ bench.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-$ECC="gcc -mcpu=v9"
+ECC="gcc -mcpu=v9"
+comp="./ecc"
+
 echo "---- Benchmarks"
 for dir in `ls benchmarks`; do
    echo "${dir}: "
    FILES=`ls benchmarks/${dir}/*.ev`
    for ev in $FILES; do
-      il=`echo $ev | sed 's/\.ev/\.il/'`
-      make $il
-      rm $il
+      $comp $ev
+      s=`echo $ev | sed 's/\.ev/\.s/'`
+      $ECC $s
+      benchmarks/${dir}/a.out < benchmarks/${dir}/input >&! benchmarks/${dir}/output.ev
+      diff -wb output.ev output >&! /dev/null
+      echo "$dir : $?"
    done
 done
