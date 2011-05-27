453db2a0e1f88a3916bbba6cbdaff3c3ed2d84d6
pharaxe <bsweedler@gmail.com>
2011-05-26 19:01:24 -0700

    tests.sh now tells you how many tests passed, and what tests failed.

--- test.sh
+++ test.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
 c=`ls -l benchmarks/ | wc -l`
+c=`expr $c - 1` # fixes off by one error.
+failed=""
+passed=0
 
 echo "-- Running all Benchmarks..."
 for i in `seq 1 $c`; do
-   echo $i | ./build.sh
+   echo $i | ./build.sh -q
+
+   if [ $? = 0 ]; then
+      passed=`expr $passed + 1`
+   else
+      if [ "$failed" != "" ]; then
+         failed="${failed}, ${i}"
+      else 
+         failed="$i"
+      fi
+   fi
 done
+
+echo ""
+echo "-- Passed $passed tests."
+if [ "$failed" != "" ]; then
+   echo "-- Failed these tests: ${failed}"
+fi
