71761e794cfacb5fbac8035b69a51f9eeb61042f
Nat Welch <nat@natwelch.com>
2011-05-26 22:45:50 -0700

    Better test output.

--- build.sh
+++ build.sh
@@ -4,11 +4,10 @@ ECC="gcc -mcpu=v9"
 comp="./ecc"
 passed=1
 
-if [ "$1" != "-q" ]; then
-   echo " --- Select a test case to run."
-fi
+# Actually does our work.
+function build() {
+   dir=$1
 
-select dir in `ls benchmarks`; do
    echo -e "\n${dir}: "
    FILES=`ls benchmarks/${dir}/${dir}.ev`
    for ev in $FILES; do
@@ -27,4 +26,16 @@ select dir in `ls benchmarks`; do
    done
 
    exit $passed
-done
+}
+
+# Either ask the user, or pass the dir name from stdin on.
+if [ "$1" != "-q" ]; then
+   echo " --- Select a test case to run."
+   select dir in `ls benchmarks`; do
+      build $dir
+   done
+else
+   read d
+   build $d
+fi
+
