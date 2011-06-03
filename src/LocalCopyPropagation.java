f61cc79f08a38c7467f5a4c259f715108f5d7a69
pharaxe <bsweedler@gmail.com>
2011-06-02 18:15:38 -0700

    Forgot to add file to repository.

---
+++ src/LocalCopyPropagation.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class LocalCopyPropagation {
+
+   public void optimize(Node block) {
+      Map<Register, Register> validCopies = new HashMap<Register, Register>();
+      Instruction instr, temp;
+      Register src, dest, betterSrc;
+      ArrayList<Instruction> instructions = block.getInstr();
+
+      for (int ndx = 0; ndx < instructions.size(); ndx++) {
+         instr = instructions.get(ndx);
+
+         dest = null;
+         src = null;
+
+         if (!instr.getSources().isEmpty()) {
+            for (Register oldSource : instr.getSources()) {
+               betterSrc = validCopies.get(oldSource);
+
+               if (betterSrc != null) {
+                  instr.transformSources(oldSource, betterSrc);
+               }
+            }
+         }
+
+         if (!instr.getDestinations().isEmpty()) {
+            dest = instr.getDestinations().get(0);
+
+            // Don't need to kill on useless moves.
+            if (!(dest.equals(src) && instr instanceof MovInstruction)) {
+               validCopies.remove(dest);
+            }
+         }
+
+         if (instr instanceof MovInstruction && !(dest.equals(src))) {
+            src = instr.getSources().get(0);
+            validCopies.put(dest, src);
+         }
+      }
+   }
+}
